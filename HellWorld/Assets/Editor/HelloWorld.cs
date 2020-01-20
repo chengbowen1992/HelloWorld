a5dde1dcef93ed68b3f5d3d01dcc49c4930ce131
chengbowen1992 <samurai54@outlook.com>
2020-01-20 11:50:37 +0800

    添加打包脚本

---
+++ HellWorld/Assets/Editor/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class HelloWorld : Editor
+{
+    [MenuItem("Build/Build Android")]
+    public static void CmdBuild()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+        
+        List<string> sceneList = new List<string>();
+        EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
+        for (int i = 0; i < temp.Length; i++)
+        {
+            sceneList.Add(temp[i].path);
+        }
+
+        BuildPipeline.BuildPlayer(sceneList.ToArray(), "./HelloWorld.apk", BuildTarget.Android, BuildOptions.None);
+        
+    }
+}
